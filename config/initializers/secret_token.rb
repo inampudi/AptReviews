717355efdd23a16040dd0dee2dfeacbb597984b0
mahi <mahi.inampudi@gmail.com>
2013-01-25 14:08:03 -0600

    Skeleton app
    Auto generated using commands.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AptReviews::Application.config.secret_token = 'ff874d22427ce92f1265b38e63291a94edf14db38b1832b649e481b6ea74e1743d442c8b3da2b34ff24073d60cb1defdd85e016c90d57b5a766a06bda630a4d1'
